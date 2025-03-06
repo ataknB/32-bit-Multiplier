6d5c6ab7d0201a7076afcadebd75effe7adfe77b
ataknB <beyenatakanm@gmail.com>
2025-03-06 23:00:26 +0300

    32-bit multiplier

--- Multiplier_Circuit.sv
+++ Multiplier_Circuit.sv
@@ -1,255 +1,192 @@
-module Multiplier_4bit #(
-    parameter WIDTH = 4
-    )(
+module Multiplier_32bit(
     //input logic clk,
     //input logic rst,
-    input logic [WIDTH-1:0] in0, // multiplicand
-    input logic [WIDTH-1:0] in1, // multiplier
+    input logic [31:0] in0, // multiplicand
+    input logic [31:0] in1, // multiplier
     output logic done,
-    output logic [7:0] result
+    output logic [63:0] result
 );
+    genvar i;
 
-    // Phase registers
-    logic [3:0]Holder_0;
+    logic [31:0]Holder[31:0];
+    logic [31:0]Holder_wire[31:0];
 
-    logic add_en_1;
-    logic sub_en_1;
-    logic [4:0]Q_1;
-    
-    logic [8:0]phase_result_1;
-    logic [8:0]phase_result_1_wire;
-    logic [3:0]Holder_1_wire;
-    logic [3:0]Holder_1;
+    logic [31:0]add_en;
+    logic [31:0]sub_en;
 
-    logic add_en_2;
-    logic sub_en_2;
-    logic [4:0] Q_2;
-    
-    logic [8:0] phase_result_2;
-    logic [8:0]phase_result_2_wire;
-    logic [3:0]Holder_2_wire;
-    logic [3:0] Holder_2;
-
-    logic add_en_3;
-    logic sub_en_3;
-    logic [4:0] Q_3;
-    
-    logic [8:0] phase_result_3;
-    logic [8:0]phase_result_3_wire;
-    logic [3:0] Holder_3_wire;
-    logic [3:0] Holder_3;
-    
-    logic add_en_4;
-    logic sub_en_4;
-    logic [4:0] Q_4;
-    logic [8:0] phase_result_4;
-    logic [8:0]phase_result_4_wire;
-    logic [3:0] Holder_4_wire;
-    logic [3:0] Holder_4;
+    logic [32:0]Q[31:0];
+    logic [64:0]phase_result_wire[31:0];
+    logic [64:0]phase_result[31:0];
     
 
-    /*
-    logic [4:0] Q_1, Q_2, Q_3, Q_4;
-    logic [3:0] Holder_0, Holder_1, Holder_2, Holder_3, Holder_4;
-    
-    // Control signals
-    logic add_en_1, add_en_2, add_en_3, add_en_4;
-    logic sub_en_1, sub_en_2, sub_en_3, sub_en_4;
-    
-    // Intermediate results
-    logic [8:0] phase_result_1, phase_result_2, phase_result_3, phase_result_4;
-    */
-
-    // Phase 1
     always_comb begin
-        Q_1 = {in1, 1'b0};    // Initial Q with extra bit
-        Holder_0 = 4'd0;     // Initial accumulator
+        Q[0] = {in1, 1'b0};    // Initial Q with extra bit
+        Holder[0] = 32'd0;     // Initial accumulator
 
-        case({Q_1[1], Q_1[0]})
-            2'b00: begin sub_en_1 = 0; add_en_1 = 0; end
-            2'b01: begin sub_en_1 = 0; add_en_1 = 1; end
-            2'b10: begin sub_en_1 = 1; add_en_1 = 0; end
-            2'b11: begin sub_en_1 = 0; add_en_1 = 0; end
+        case({Q[0][1], Q[0][0]})
+            2'b00: begin sub_en[0] = 0; add_en[0] = 0; end
+            2'b01: begin sub_en[0] = 0; add_en[0]  = 1; end
+            2'b10: begin sub_en[0] = 1; add_en[0]  = 0; end
+            default: begin sub_en[0] = 0; add_en[0]  = 0; end
         endcase
 
-        if(add_en_1) begin
-            Holder_1_wire = Holder_0 + in0;
+        if(add_en[0]) begin
+            Holder_wire[0] = Holder[0] + in0;
         end
-        else if(sub_en_1) begin
-            Holder_1_wire = Holder_0 - in0;
+        else if(sub_en[0]) begin
+            Holder_wire[0] = Holder[0] - in0;
         end
         else begin
-            Holder_1_wire = Holder_0;
+            Holder_wire[0] = Holder[0];
         end
 
-        phase_result_1_wire = {Holder_1_wire, Q_1};
-        phase_result_1 = {phase_result_1_wire[8] , phase_result_1_wire[8:1]};
+        phase_result_wire[0] = {Holder_wire[0], Q[0]};
+        phase_result[0] = {phase_result_wire[0][64] , phase_result_wire[0][64:1]};
 
     end
 
-    // Phase 2
-    always_comb begin
-        Q_2 = {phase_result_1[4:0]};    // Initial Q with extra bit
-        Holder_1 = phase_result_1[8:5];     // Initial accumulator
+    generate 
+        for(i=1 ; i<8 ; i=i+1)
+        begin
+            always_comb begin
+            Q[i] = phase_result[i-1][32:0];    // Initial Q with extra bit
+            Holder[i] = phase_result[i-1][64:33];     // Initial accumulator
 
-        case({Q_2[1], Q_2[0]})
-            2'b00: begin sub_en_2 = 0; add_en_2 = 0; end
-            2'b01: begin sub_en_2 = 0; add_en_2 = 1; end
-            2'b10: begin sub_en_2 = 1; add_en_2 = 0; end
-            2'b11: begin sub_en_2 = 0; add_en_2 = 0; end
-        endcase
+            case({Q[i][1], Q[i][0]})
+                2'b00: begin sub_en[i] = 0; add_en[i] = 0; end
+                2'b01: begin sub_en[i] = 0; add_en[i]  = 1; end
+                2'b10: begin sub_en[i] = 1; add_en[i]  = 0; end
+                default: begin sub_en[i] = 0; add_en[i]  = 0; end
+            endcase
 
-        if(add_en_2) begin
-            Holder_2_wire = Holder_1 + in0;
+            if(add_en[i]) begin
+                Holder_wire[i] = Holder[i] + in0;
+            end
+            else if(sub_en[i]) begin
+                Holder_wire[i] = Holder[i] - in0;
+            end
+            else begin
+                Holder_wire[i] = phase_result[i-1][64:33];
+            end
+
+            phase_result_wire[i] = {Holder_wire[i], Q[i]};
+            phase_result[i] = {phase_result_wire[i][64] , phase_result_wire[i][64:1]};
+
+            end
         end
-        else if(sub_en_2) begin
-            Holder_2_wire = Holder_1 - in0;
+    endgenerate
+
+
+    generate 
+        for(i=8 ; i<16 ; i=i+1)
+        begin
+            always_comb begin
+            Q[i] = phase_result[i-1][32:0];    // Initial Q with extra bit
+            Holder[i] = phase_result[i-1][64:33];     // Initial accumulator
+
+            case({Q[i][1], Q[i][0]})
+                2'b00: begin sub_en[i] = 0; add_en[i] = 0; end
+                2'b01: begin sub_en[i] = 0; add_en[i]  = 1; end
+                2'b10: begin sub_en[i] = 1; add_en[i]  = 0; end
+                default: begin sub_en[i] = 0; add_en[i]  = 0; end
+            endcase
+
+            if(add_en[i]) begin
+                Holder_wire[i] = Holder[i] + in0;
+            end
+            else if(sub_en[i]) begin
+                Holder_wire[i] = Holder[i] - in0;
+            end
+            else begin
+                Holder_wire[i] = phase_result[i-1][64:33];
+            end
+
+            phase_result_wire[i] = {Holder_wire[i], Q[i]};
+            phase_result[i] = {phase_result_wire[i][64] , phase_result_wire[i][64:1]};
+
+            end
         end
-        else begin
-            Holder_2_wire = phase_result_1[8:5];
+    endgenerate
+
+
+
+    generate 
+        for(i=16 ; i<24 ; i=i+1)
+        begin
+            always_comb begin
+            Q[i] = phase_result[i-1][32:0];    // Initial Q with extra bit
+            Holder[i] = phase_result[i-1][64:33];    // Initial accumulator
+
+            case({Q[i][1], Q[i][0]})
+                2'b00: begin sub_en[i] = 0; add_en[i] = 0; end
+                2'b01: begin sub_en[i] = 0; add_en[i]  = 1; end
+                2'b10: begin sub_en[i] = 1; add_en[i]  = 0; end
+                default: begin sub_en[i] = 0; add_en[i]  = 0; end
+            endcase
+
+            if(add_en[i]) begin
+                Holder_wire[i] = Holder[i] + in0;
+            end
+            else if(sub_en[i]) begin
+                Holder_wire[i] = Holder[i] - in0;
+            end
+            else begin
+                Holder_wire[i] = phase_result[i-1][64:33];
+            end
+
+            phase_result_wire[i] = {Holder_wire[i], Q[i]};
+            phase_result[i] = {phase_result_wire[i][64] , phase_result_wire[i][64:1]};
+
+            end
         end
+    endgenerate
 
-        phase_result_2_wire = {Holder_2_wire, Q_2};
-        phase_result_2 = {phase_result_2_wire[8] , phase_result_2_wire[8:1]};
 
-    end
 
-    // Phase 3
-    always_comb begin
-        Q_3 = {phase_result_2[4:0]};    // Initial Q with extra bit
-        Holder_2 = phase_result_2[8:5];     // Initial accumulator
+    generate 
+        for(i=24 ; i<32 ; i=i+1)
+        begin
+            always_comb begin
+            Q[i] = phase_result[i-1][32:0];    // Initial Q with extra bit
+            Holder[i] = phase_result[i-1][64:33];     // Initial accumulator
 
-        case({Q_3[1], Q_3[0]})
-            2'b00: begin sub_en_3 = 0; add_en_3 = 0; end
-            2'b01: begin sub_en_3 = 0; add_en_3 = 1; end
-            2'b10: begin sub_en_3 = 1; add_en_3 = 0; end
-            2'b11: begin sub_en_3 = 0; add_en_3 = 0; end
-        endcase
+            case({Q[i][1], Q[i][0]})
+                2'b00: begin sub_en[i] = 0; add_en[i] = 0; end
+                2'b01: begin sub_en[i] = 0; add_en[i]  = 1; end
+                2'b10: begin sub_en[i] = 1; add_en[i]  = 0; end
+                default: begin sub_en[i] = 0; add_en[i]  = 0; end
+            endcase
 
-        if(add_en_3) begin
-            Holder_3_wire = Holder_2 + in0;
+            if(add_en[i]) begin
+                Holder_wire[i] = Holder[i] + in0;
+            end
+            else if(sub_en[i]) begin
+                Holder_wire[i] = Holder[i] - in0;
+            end
+            else begin
+                Holder_wire[i] = phase_result[i-1][64:33];
+            end
+
+            phase_result_wire[i] = {Holder_wire[i], Q[i]};
+            phase_result[i] = {phase_result_wire[i][64] , phase_result_wire[i][64:1]};
+
+            end
         end
-        else if(sub_en_3) begin
-            Holder_3_wire = Holder_2 - in0;
-        end
-        else begin
-            Holder_3_wire = phase_result_2[8:5];
-        end
+    endgenerate
 
-        phase_result_3_wire = {Holder_3_wire, Q_3};
-        phase_result_3 = {phase_result_3_wire[8] , phase_result_3_wire[8:1]};
-
-    end
-
-    // Phase 4
-    always_comb begin
-        Q_4 = {phase_result_3[4:0]};    // Initial Q with extra bit
-        Holder_3 = phase_result_3[8:5];     // Initial accumulator
-
-        case({Q_4[1], Q_4[0]})
-            2'b00: begin sub_en_4 = 0; add_en_4 = 0; end
-            2'b01: begin sub_en_4 = 0; add_en_4 = 1; end
-            2'b10: begin sub_en_4 = 1; add_en_4 = 0; end
-            2'b11: begin sub_en_4 = 0; add_en_4 = 0; end
-        endcase
-
-        if(add_en_4) begin
-            Holder_4_wire = Holder_3 + in0;
-        end
-        else if(sub_en_4) begin
-            Holder_4_wire = Holder_3 - in0;
-        end
-        else begin
-            Holder_4_wire = phase_result_3[8:5];
-        end
-
-        phase_result_4_wire = {Holder_4_wire, Q_4};
-        phase_result_4 = {phase_result_4_wire[8] , phase_result_4_wire[8:1]};
-
-    end
-
-    // Output assignment
-    assign result = phase_result_4[8:1]; // Take upper 8 bits
+    assign result = phase_result[31][63:1]; // Take upper 8 bits
     assign done = 1'b1; // Combinational, so always done
 
 endmodule
 
+/*
+module Multiplier_Reg(
+    input logic clk,
+    input logic rst,  
 
+    input logic in,
 
-
-module Ripple_Carry_Adder(
-
-    input logic in0,
-    input logic in1,
-
-    input logic carry_in,
-    output logic carry_out,
-    output logic result
-
-    );
-    
-    logic wire_bit;
-
-    assign wire_bit = in0 ^ in1;
-
-    assign result = wire_bit ^ carry_in;
-    assign carry_out = (wire_bit & carry_in) | (in0 & in1);
-
+    input logic out,
+);
 endmodule
-
-
-module Kogge_Stone_4bit(
-    input logic [3:0]in0,
-    input logic [3:0]in1,
-
-    input logic carry_in,
-
-    output logic carry_out,
-    output logic [3:0]result
-    );
-
-    genvar i;
-
-    logic [8:0]propagate_bit;
-    logic [8:0]generate_bit;
-
-    logic [4:0]carry_bit;
-
-    generate
-        for(i = 0; i<4 ; i=i+1)
-        begin
-            assign propagate_bit[i] = in0[i] ^^ in1[i];
-            assign generate_bit[i] = in0[i] && in1[i];
-        end
-    endgenerate
-
-    //stage 1
-    assign propagate_bit[4] = propagate_bit[1] ^^ propagate_bit[0];
-    assign propagate_bit[5] = propagate_bit[2] ^^ propagate_bit[1];
-    assign propagate_bit[6] = propagate_bit[3] ^^ propagate_bit[2];
-
-    assign generate_bit[4] = generate_bit[1] || (propagate_bit[1] && generate_bit[0]);
-    assign generate_bit[5] = generate_bit[2] || (propagate_bit[2] && generate_bit[1]);
-    assign generate_bit[6] = generate_bit[3] || (propagate_bit[3] && generate_bit[2]);
-
-    //stage 2
-    assign propagate_bit[7] = propagate_bit[0] ^^ propagate_bit[5];
-    assign propagate_bit[8] = propagate_bit[4] ^^ propagate_bit[6];
-
-    assign generate_bit[7] = generate_bit[5] || (propagate_bit[5] && generate_bit[0]);
-    assign generate_bit[8] = generate_bit[6] || (propagate_bit[6] && generate_bit[4]);
-
-    //carry 
-    assign carry_bit[0] = carry_in;
-    assign carry_bit[1] = generate_bit[4] || (propagate_bit[4] && carry_in);
-    assign carry_bit[2] = generate_bit[7] || (propagate_bit[7] && carry_in);
-    assign carry_bit[3] = generate_bit[8] || (propagate_bit[8] && carry_in);
-
-    //sum
-    assign result[0] = propagate_bit[0] ^^ carry_bit[0];
-    assign result[1] = propagate_bit[1] ^^ carry_bit[0];
-    assign result[2] = propagate_bit[2] ^^ carry_bit[0];
-    assign result[3] = propagate_bit[3] ^^ carry_bit[0];
-    
-
-endmodule
+*/
--- tb.sv
+++ tb.sv
@@ -1,19 +1,17 @@
 `timescale 1ns/1ps
 
-module Multiplier_4bit_tb;
+module Multiplier_32bit_tb;
 
     // Testbench signals
     //logic clk;
     //logic rst;
-    logic [3:0] in0;    // multiplicand
-    logic [3:0] in1;    // multiplier
+    logic [31:0] in0;    // multiplicand
+    logic [31:0] in1;    // multiplier
     logic done;
-    logic [7:0] result;
+    logic [63:0] result;
 
     // Instantiate the DUT (Device Under Test)
-    Multiplier_4bit #(
-        .WIDTH(4)
-    ) dut (
+    Multiplier_32bit TB (
         //.clk(clk),
         //.rst(rst),
         .in0(in0),
@@ -44,28 +42,28 @@ module Multiplier_4bit_tb;
         // Test cases
         // Test 1: 3 * 2 = 6
         //#10;
-        in0 = 4'd2;
-        in1 = 4'd3;
+        in0 = 32'd6;
+        in1 = 32'd6;
         #20;
 
-        in0 = 4'd5;
-        in1 = 4'd3;
+        in0 = 32'd5;
+        in1 = 32'd3;
         #20;
 
-        in0 = 4'd7;
-        in1 = 4'd2;
+        in0 = 32'd7;
+        in1 = 32'd2;
         #20;
 
-        in0 = 4'd4;
-        in1 = 4'd4;
+        in0 = 32'd4;
+        in1 = 32'd4;
         #20;
 
-        in0 = 4'd0;
-        in1 = 4'd5;
+        in0 = 32'd0;
+        in1 = 32'd5;
         #20;
 
-        in0 = 4'd15;
-        in1 = 4'd1;
+        in0 = 32'd15;
+        in1 = 32'd1;
         #20;
 
         $finish;
